371a5688b7b3987341996ecffa4bc5d0f0839195
rb150 <rbajaj852@gmail.com>
2019-03-06 23:46:53 -0600

    master2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,6 +11,7 @@ in this case, title.
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    @yield('styles')
 </head>
 <body>
     
